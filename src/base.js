0accddc57a3ad5d4c006795cf478ebf523420f76
ishatr <isha212195@gmail.com>
2017-08-28 01:51:59 +0530

    add initial code :fire:

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass ({
+    apiKey: "AIzaSyBLigpXuXCfJQ8XCoBDF0FzCfw0UahJVAg",
+    authDomain: "catch-of-the-day-a2679.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-a2679.firebaseio.com"
+});
+
+export default base;
